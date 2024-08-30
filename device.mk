6a8fcb881ee626ff3f3ac266fdc9281be774c931
Captain Throwback <captainthrowback@hotmail.com>
2024-08-30 11:04:49 +0700

    judypn: Force keymaster version
    With recent updates, keymaster version auto-detection is
    non-functional on some devices. Forcing keymaster version
    prevents the device from being stuck at boot and allows
    decryption to proceed.
    
    Change-Id: I615306b729b6572edd527cfb3550a67c84dce338

--- device.mk
+++ device.mk
@@ -51,6 +51,9 @@ PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    keymaster_ver=3.0
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
